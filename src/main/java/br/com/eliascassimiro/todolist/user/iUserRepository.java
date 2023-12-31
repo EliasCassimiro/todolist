0a98c8636d3ded5f2fcacc40aacd65f468818e3a
EliasCassimiro <eliascassimiro1948@outlook.com>
2023-12-31 20:38:46 -0300

    atualização dos arquivos de users

--- src/main/java/br/com/eliascassimiro/todolist/user/iUserRepository.java
+++ src/main/java/br/com/eliascassimiro/todolist/user/iUserRepository.java
@@ -4,8 +4,7 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface iUserRepository extends JpaRepository<userModel, UUID>{
 
-    
-    
+public interface iUserRepository extends JpaRepository<userModel, UUID>{
+    userModel findByUserName(String userName);;
 }
