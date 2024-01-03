de49b2948e6640aec29996f9c6a6fb1fe480b31e
EliasCassimiro <eliascassimiro1948@outlook.com>
2024-01-02 21:42:06 -0300

    Adicionando arquivos na pasta Task
    taskController
    iTaskRepository

---
+++ src/main/java/br/com/eliascassimiro/todolist/task/taskController.java
@@ -0,0 +1,20 @@
+package br.com.eliascassimiro.todolist.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+public class taskController {
+    @Autowired
+    private iTaskRepository taskRepository;
+
+    @PostMapping("/")
+    public taskModel create(@RequestBody taskModel taskModel){
+        var task = this.taskRepository.save(taskModel);
+        return task;
+    }
+}
