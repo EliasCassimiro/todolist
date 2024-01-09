341285ba6b49443a0494c7e2489b04cb38bc01b1
EliasCassimiro <eliascassimiro1948@outlook.com>
2024-01-08 21:44:36 -0300

    atualização no filterTaskAuth

--- src/main/java/br/com/eliascassimiro/todolist/filter/filterTaskAuth.java
+++ src/main/java/br/com/eliascassimiro/todolist/filter/filterTaskAuth.java
@@ -1,26 +1,51 @@
 package br.com.eliascassimiro.todolist.filter;
-
+ 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class filterTaskAuth implements Filter{
+public class filterTaskAuth extends OncePerRequestFilter{
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         
         //EXECUTAR ALGUMA AÇÃO
-        System.out.println("Chegou no Filtro");
-        chain.doFilter(request, response);
+        /*System.out.println("Chegou no Filtro");
+        chain.doFilter(request, response);*/
+
+        //PEGAR AUTENTICAÇÃO (USUARIO E SENHA)
+
+        var authorization =  request.getHeader("Authorization");
+
+        var authEncoded = authorization.substring("Basic".length()).trim();
+
+        byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+       
+        var authString = new String(authDecode);
+        
+        String[] credentials = authString.split(":");
+        String username = credentials[0];
+        String password = credentials[1];
+
+        System.out.println("Authorization");
+        System.out.println(username);
+        System.out.println(password);
+
+        //VALIDAR USUARIO
+        //VALIDAR SENHA
+        //SEGUE VIAGEM
+        filterChain.doFilter(request, response);
             
     }
-
     
 }
