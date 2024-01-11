4a42defc586148fceee5218c9f201008ac8b1d2b
EliasCassimiro <eliascassimiro1948@outlook.com>
2024-01-11 19:55:59 -0300

    update de tarefas

--- src/main/java/br/com/eliascassimiro/todolist/task/iTaskRepository.java
+++ src/main/java/br/com/eliascassimiro/todolist/task/iTaskRepository.java
@@ -1,9 +1,12 @@
 package br.com.eliascassimiro.todolist.task;
 
+import java.util.List;
 import java.util.UUID;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface iTaskRepository extends JpaRepository<taskModel, UUID>{
-    
+    List<taskModel>findByIdUser(UUID idUser);
+
 }
